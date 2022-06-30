c0bf439e386297c4a7ac35ce8d1fc1b8c6eb2e0e
kionou <kionoumamadou.00@gmail.com>
2022-06-30 08:52:44 +0000

    08h52

--- database/connecterData.js
+++ database/connecterData.js
@@ -1,8 +1,13 @@
 const  Sequelize  = require("sequelize")
 
-let sequelize = new Sequelize("migration_sequelize","kionou","12345",{
+// let sequelize = new Sequelize("migration_sequelize","kionou","12345",{
+//     dialect:'mysql',
+//     host:'192.168.64.2',
+// })
+
+let sequelize = new Sequelize("migration_sequelize","root","",{
     dialect:'mysql',
-    host:'192.168.64.2',
+    host:'localhost',
 })
 
 module.exports=sequelize;
